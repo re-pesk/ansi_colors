221e77bda54827c8cd62bb045fb2d165755e3431
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2025-08-24 20:14:12 +0300

    Update tests

--- tests/_test_helpers.sh
+++ tests/_test_helpers.sh
@@ -1,6 +1,16 @@
 nl='
 '
 
+test_countLines() (
+  i=0
+  while IFS= read -r line; do
+    i=$((i + 1))
+  done <<EOF
+$1
+EOF
+  echo "$i"
+)
+
 test_firstLine() (
   while IFS= read -r line; do
     echo "$line"
@@ -23,8 +33,6 @@ EOF
 test_getLine() (
   i=0
   while IFS= read -r line; do
-    # echo "line='$line'\n"
-    # echo "i='$i'\n"
     [ "$i" -eq "$2" ] && echo "$line" && break
     i=$((i + 1))
   done <<EOF
@@ -33,23 +41,27 @@ EOF
 )
 
 test_shell_title() {
-  printf "\033[35m|\033[04;53m        $(ansi_getShellName)        \033[55;24m|\033[39m\n" >&2
+  printf "\n\033[35m|\033[04;53m        $(ansi_getShellName)        \033[55;24m|\033[39m\n" >&2
 }
 
 test_title() {
-  printf "\033[04;35m%s\033[00m\n" "$*" >&2
+  printf "\n\033[21;35m%s\033[39;24m\n\n" "$*" >&2
+}
+
+test_important() {
+  printf "\n\033[04;35m%s\033[39;24m\n\n" "$*" >&2
 }
 
 test_message() {
-  printf "\033[35m%s\033[00m\n" "$*" >&2
+  printf "\n\033[35m%s\033[39m\n\n" "$*" >&2
 }
 
 test_success() {
-  printf "\033[32m✔ Success! %s\033[00m\n" "$*" >&2
+  printf "\033[32m✔ Success! %s\033[39m\n" "$*" >&2
 }
 
 test_failure() {
-  printf "\033[31m✖ Failure! %s\033[00m\n" "$*" >&2
+  printf "\033[31m✖ Failure! %s\033[39m\n" "$*" >&2
 }
 
 test_assert() {
@@ -82,8 +94,7 @@ test_printColors() {
     done
   )$(ansi_sequence ${default_color})"
 
-  ansi_text "${ansi_codes}" "${output}"
-  echo
+  ansi_text "${ansi_codes}" "${output}"; echo
 
   output="$(
     for c; do
@@ -91,8 +102,7 @@ test_printColors() {
     done
   )$(ansi_sequence ${default_color})"
 
-  ansi_text "${off}-${off}" "${output}"
-  echo
+  ansi_text "${off}-${off}" "${output}"; echo
 }
 
 test_printExtColors() {
@@ -107,10 +117,8 @@ test_printExtColors() {
 
 test_colors() {
   test_title "$1: $(ansi_getOnCodes $2);{$3..$4}"
-  echo >&2
   actual=$(test_printColors "$2" $(seq $3 $4))
-  [ "$show_actual" -gt 0 ] && printf "$actual\n" >&2
-  echo >&2
+  [ "$show_actual" -gt 0 ] && printf "$actual\n\n" >&2
   test_assert "$actual\n" "$5" "$1"
   [ "$?" -gt 0 ] && echo 1 && return 1
   echo 0
@@ -124,33 +132,79 @@ test_extColors() {
   echo 0
 }
 
-test_report () {
+test_runTests() (
   sum=0
-  no=0
+  num=0
   for result in $($1); do
-    no=$((no + 1))
+    num=$((num + 1))
     sum=$((sum + result))
   done
+  echo "${num}-${sum}"
+)
+
+test_reportTests() (
+  test_group_list="$1"
+  test_shell_title
+
+  num=0
+  sum=0
+  for line in $test_group_list; do
+    result="$(test_runTests "$line")"
+    tests_num=${result%-*}
+    tests_sum=${result#*-}
+    num=$((num + ${tests_num}))
+    sum=$((sum + ${tests_sum}))
+    output_func="test_success"
+    if [ "${tests_sum}" -gt 0 ]; then
+      output_func="test_failure"
+    fi
+    test_message "The results of running '$line':"
+    $output_func "Results: total: ${tests_num}, passed: $((tests_num - tests_sum)), failed: $tests_sum."
+  done
+  test_important "The total results of all tests running on '$shell_name':"
   output_func="test_success"
   if [ "$sum" -gt 0 ]; then
     output_func="test_failure"
-    echo
   fi
-  test_message "The results of '$1':"; echo >&2
-  $output_func "Results: total: ${no}, passed: $((no - sum)), failed: $sum."
-  echo >&2
-}
+  $output_func "Results: total: ${num}, passed: $((num - sum)), failed: $sum."
+  [ "${sum}" -gt 0 ] && return 1
+  return 0
+)
 
 shell_is_available() (
   shell_name="$1"
   result="$(which "${shell_name}")"
-  [ "${#result}" -lt 1 ] && echo "The '${shell_name}' is not installed!" && return 1
+  [ "${#result}" -lt 1 ] && test_failure "The '${shell_name}' is not installed!" && return 1
   return 0
 )
 
 run_script() {
   shell_name="$1"
-  file_name="$2"
+  test_file="$2"
   shell_is_available "${shell_name}"
-  [ "$?" -eq 0 ] && "${shell_name}" "${file_name}"
+  [ "$?" -gt 0 ] && return 1
+  "${shell_name}" "${test_file}"
+  return $?
+}
+
+run_tests_on_shells() {
+  shell_list="$1"
+  test_file="$2"
+  clear
+
+  message_list=""
+  for shell_name in $shell_list; do
+    message=""
+    result="$(run_script "$shell_name" "$test_file")"
+    if [ "$?" -gt 0 ]; then
+      message="$(test_failure "Tests on shell '$shell_name' are failed!" 2>&1)"
+    else
+      message="$(test_success "Tests on shell '$shell_name' are passed!" 2>&1)"
+    fi
+    message_list="${message_list:+${message_list}${nl}}${message}"
+  done
+
+  test_message "====== Tests are finished! ======"
+
+  echo "${message_list}${nl}" >&2
 }
--- tests/tests.sh
+++ tests/tests.sh
@@ -4,13 +4,8 @@ DIR_TESTS="$(cd "$(dirname "${0}")" && pwd)"
 
 . "${DIR_TESTS}/_test_helpers.sh"
 
-file_name="${DIR_TESTS}/_tests.sh"
+test_file="${DIR_TESTS}/_tests.sh"
 
-clear
+shell_list="dash yash bash ksh zsh osh"
 
-run_script dash "${file_name}"
-run_script yash "${file_name}"
-run_script bash "${file_name}"
-run_script ksh "${file_name}"
-run_script zsh "${file_name}"
-run_script osh "${file_name}"
+run_tests_on_shells "$shell_list" "$test_file"
